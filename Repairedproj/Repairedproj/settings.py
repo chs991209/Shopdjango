24fde2e7761373eeabe1ce8a5ba40dcc4b9b3fcd
Choihyunsu <chs991209@gmail.com>
2021-08-11 14:03:34 +0900

    INSTALLED_APPS에 콤마 추가

--- Repairedproj/Repairedproj/settings.py
+++ Repairedproj/Repairedproj/settings.py
@@ -38,7 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'django.contrib.humanize',
-    'rest_framework'
+    'rest_framework',
     'customer',
     'order',
     'product',
