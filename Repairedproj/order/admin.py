a31cb5a36ad40c45e23008840b2d0b09a6922941
Choihyunsu <chs991209@gmail.com>
2021-09-07 10:03:02 +0900

    MODEL에 order 상태 tuple 추가, admin에 order 상태 목록 색상 분류 추가

--- Repairedproj/order/admin.py
+++ Repairedproj/order/admin.py
@@ -1,11 +1,27 @@
 from django.contrib import admin
 from .models import Order
-
+from django.utils.html import format_html
 
 # Register your models here.
 
+
 class OrderAdmin(admin.ModelAdmin):
-    list_display = ('customer', 'product', 'status')
+    list_filter = ('status',)
+    list_display = ('customer', 'product', 'styled_status')
+
+    def styled_status(self, obj):
+        if obj.status == 'REFUNDED':
+            return format_html(f'<span style="color:red">{obj.status}</b>')
+        if obj.status == 'STANDBY':
+            return format_html(f'<span style="color:blue">{obj.status}</b>')
+        if obj.status == 'PAID':
+            return format_html(f'<span style="color:green">{obj.status}</b>')
+        if obj.status == 'PROCESSING':
+            return format_html(f'<span style="color:brown">{obj.status}</b>')
+
+        return obj.status
+
+    styled_status.short_description = 'STATUS'
 
 
 admin.site.register(Order, OrderAdmin)
--- Repairedproj/order/models.py
+++ Repairedproj/order/models.py
@@ -7,7 +7,14 @@ class Order(models.Model):
     customer = models.ForeignKey('customer.Customer', on_delete=models.CASCADE, verbose_name='customer')
     product = models.ForeignKey('product.Product', on_delete=models.CASCADE, verbose_name='Order_Prod_NAME')
     quantity = models.IntegerField(verbose_name='QUANTITY')
-    status = models.CharField(default='STANDBY', max_length=32, verbose_name='STATUS')
+    status = models.CharField(
+        choices=(
+            ('STANDBY', 'STANDBY'),
+            ('PROCESSING', 'PROCESSING'),
+            ('PAID', 'PAID'),
+            ('REFUNDED', 'REFUNDED'),
+        ),
+        default='STANDBY', max_length=32, verbose_name='STATUS')
     memo = models.TextField(null=True, blank=True, verbose_name='MEMO')
     registered_date = models.DateTimeField(auto_now_add=True, verbose_name='REGISTERED_DATE')
 
