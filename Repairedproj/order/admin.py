8fca588fb482cfd8e88fd616e7d96610eb451bb6
Choihyunsu <chs991209@gmail.com>
2021-09-13 09:44:23 +0900

    Order admin tag 변경, Product admin display 추가

--- Repairedproj/order/admin.py
+++ Repairedproj/order/admin.py
@@ -11,13 +11,13 @@ class OrderAdmin(admin.ModelAdmin):
 
     def styled_status(self, obj):
         if obj.status == 'REFUNDED':
-            return format_html(f'<span style="color:red">{obj.status}</b>')
+            return format_html(f'<span style="color:red">{obj.status}</p>')
         if obj.status == 'STANDBY':
-            return format_html(f'<span style="color:blue">{obj.status}</b>')
+            return format_html(f'<span style="color:blue">{obj.status}</p>')
         if obj.status == 'PAID':
-            return format_html(f'<span style="color:green">{obj.status}</b>')
+            return format_html(f'<span style="color:green">{obj.status}</p>')
         if obj.status == 'PROCESSING':
-            return format_html(f'<span style="color:brown">{obj.status}</b>') #f>>바로 입력
+            return format_html(f'<span style="color:brown">{obj.status}</p>') #f>>바로 입력
 
         return obj.status
 
--- Repairedproj/product/admin.py
+++ Repairedproj/product/admin.py
@@ -1,10 +1,30 @@
 from django.contrib import admin
 from .models import Product
+from django.utils.html import format_html
+from django.contrib.humanize.templatetags.humanize import intcomma
 # Register your models here.
 
 
 class ProductAdmin(admin.ModelAdmin):
-    list_display = ('name', 'price')
+    list_display = ('name', 'price_format', 'styled_stock')
+
+    def price_format(self, obj):
+        price = intcomma(obj.price)
+        return f'{price} $'
+
+
+    def styled_stock(self, obj):
+        if obj.stock >= 1000:
+            return format_html(f'<span style="color:blue">{obj.stock}</P>')
+        elif obj.stock <= 30:
+            return format_html(f'<span style="color:red">{obj.stock}</P>')
+
+        else:
+            return obj.stock
+
+    price_format.short_description = 'PRICE'
+
+    styled_stock.short_description = 'STOCK'
 
 
 admin.site.register(Product, ProductAdmin)
