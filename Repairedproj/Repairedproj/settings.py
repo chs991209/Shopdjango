30ba194c7e2aa20579d8d2b048d8b035d0b28c20
Mactaengoo <82556127+chs991209@users.noreply.github.com>
2022-01-17 09:42:45 +0900

    DJANG0 SECRET KEY HIDE
    to secrets.json

--- Repairedproj/Repairedproj/settings.py
+++ Repairedproj/Repairedproj/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-=3gcnm*2j911td_4))*4%)-ze^*cnzoyvoqwg=mau1rt0l_wl9'
+SECRET_KEY = get_secret("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
